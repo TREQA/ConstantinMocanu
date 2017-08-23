04e7ee264319939801978aad1994016938a6aaab
cmocanu <constantin.mocanu@tremend.com>
2017-08-23 16:50:16 +0300

    initial third commit

--- src/Test64.java
+++ src/Test64.java
@@ -5,7 +5,11 @@ public class Test64
         String str1 = "First value"; String str2 = "Second value";
         String str3 = new String("First value");
         String str4 = new String("Second value");
-        boolean retVal = str1.equals(str3); System.out.println(retVal);
+
+        boolean retVal = str1.equals(str3);
+
+        System.out.println(retVal);
+
         retVal = str1.equals(str2);
         System.out.println(retVal);
     }
---
+++ src/Test68.java
@@ -0,0 +1,13 @@
+public class Test68
+{
+    public static void main (String[]args)
+    {
+        String str = "Welcome to ITAcademy";
+
+        System.out.println("Return Value:");
+        System.out.println(str.substring(10));
+
+        System.out.println("Return Value:");
+        System.out.println(str.substring(10, 14));
+    }
+}
